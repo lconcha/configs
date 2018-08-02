6557e62827bcb97cc8e95fa0b0fde0df794140f8
Luis Concha <lconcha@gmail.com>
2018-08-01 19:20:04 -0500

    corrected apt and gdebi lines. added apt install libqt5opengl5

--- fmrilab_softwareconfig.sh
+++ fmrilab_softwareconfig.sh
@@ -3,20 +3,24 @@
 # miscelaneos a instalar
 apt install arc-theme \
   git g++ python-numpy \
-  libeigen3-dev zlib1g-dev libqt4-opengl-dev libgl1-mesa-dev libfftw3-dev libtiff5-dev \
+  libeigen3-dev zlib1g-dev libqt4-opengl-dev libgl1-mesa-dev \
+  libfftw3-dev libtiff5-dev \
+  libqt5opengl5 libqt5opengl5-dev \
   libcanberra-gtk3-module \
   chromium-browser chrome-gnome-shell \
   x2goclient sshfs \
   inkscape keepassx \
-  gdebi-core htop tree curl
+  gdebi-core htop tree curl \
   r-base \
   libmng-dev \
   libgtkglext1 \
   tcsh
+
+
 # rstudio
 #apt install r-base
 wget --progress=bar --directory-prefix=/tmp https://download1.rstudio.org/rstudio-xenial-1.1.456-amd64.deb
-gdebi /tmp/rstudio.org/rstudio-xenial-1.1.456-amd64.deb
+gdebi /tmp/rstudio-xenial-1.1.456-amd64.deb
 
 # para fsl 509
 # apt install libmng-dev
