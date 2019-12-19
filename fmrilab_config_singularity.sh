be6214f22df04f7e97c1aea5d7578db66e8f6170
Luis Concha <lconcha@gmail.com>
2019-12-19 12:55:16 -0600

    changed location of localdestdir and create subdirs

--- fmrilab_config_singularity.sh
+++ fmrilab_config_singularity.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+localstatedir=/localstatedir
 
 echo "Creating the localdestdir for singularity in host $HOST"
-mkdir -v /opt/singularity_localstatedir
+mkdir -vp \
+${localstatedir}/singularity/mnt \
+${localstatedir}/singularity/mnt/container \
+${localstatedir}/singularity/mnt/final \
+${localstatedir}/singularity/mnt/overlay \
+${localstatedir}/singularity/mnt/session
