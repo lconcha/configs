7204c207a50f3ebc2b0f06e6d39be349bf8f4494
Luis Concha <lconcha@gmail.com>
2021-11-11 12:18:53 -0600

    added xvfb and download of latest debs

--- fmrilab_softwareconfig.sh
+++ fmrilab_softwareconfig.sh
@@ -23,7 +23,8 @@ apt install arc-theme \
   tilix shutter \
   cinnamon-desktop-environment plasma-desktop budgie-desktop lxde gnome-session \
   parallel \
-  dia
+  dia \
+  xvfb
 
 #update-alternatives --config gdm3.css
 
@@ -33,13 +34,16 @@ apt install arc-theme \
 
 
 # rstudio
+echo "[installing rstudio]"
 # First we install a PPA for the latest version of R, which is more modern than that in the ubuntu repos
 add-apt-repository ppa:marutter/rrutter
 apt-get update
 apt install r-base r-base-dev
 # and now we install rstudio
-wget --progress=bar --directory-prefix=/tmp https://download1.rstudio.org/rstudio-xenial-1.1.456-amd64.deb
-gdebi /tmp/rstudio-xenial-1.1.456-amd64.deb
+#wget --progress=bar --directory-prefix=/tmp https://download1.rstudio.org/rstudio-xenial-1.1.456-amd64.deb
+wget --progress=bar --directory-prefix=/tmp https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2021.09.1-372-amd64.deb
+#gdebi /tmp/rstudio-xenial-1.1.456-amd64.deb
+gdebi /tmp/rstudio-2021.09.1-372-amd64.deb
 ln -s /usr/lib/rstudio/bin/libicui18n.so.55 /usr/lib/rstudio/bin/libicui18n.so.52
 wget --progress=bar --directory-prefix=/tmp http://mirrors.kernel.org/ubuntu/pool/main/libx/libxp/libxp6_1.0.2-1ubuntu1_amd64.deb
 gdebi /tmp/libxp6_1.0.2-1ubuntu1_amd64.deb
@@ -65,7 +69,9 @@ ln -sv $f /usr/lib/x86_64-linux-gnu/libgsl.so.0
 
 
 # rclone
-rclonedeb=rclone-v1.45-linux-amd64.deb
+echo "[installing rclone]"
+wget --progress=bar --directory-prefix=/tmp https://downloads.rclone.org/rclone-current-linux-amd64.deb
+rclonedeb=/tmp/rclone-current-linux-amd64.deb
 if [ -f $rclonedeb ]
 then
   gdebi --n $rclonedeb
@@ -74,17 +80,53 @@ else
 fi
 
 # atom
+echo "[installing atom]"
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 apt-get update
 apt install atom
 
 # visual studio
-deb=code_1.41.1-1576681836_amd64.deb
+echo "[installing visualstudio code]"
+#deb=code_1.41.1-1576681836_amd64.deb
+#if [ -f $deb ]
+#then
+#  gdebi --n $deb
+#else
+#  echo "Did not find .deb for visual studio code: $deb"
+#fi
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+apt install apt-transport-https
+apt update
+apt install code
+
+
+# google chrome
+echolor "[installing google chrome]"
+wget --progress=bar --directory-prefix=/tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+deb=/tmp/google-chrome-stable_current_amd64.deb
 if [ -f $deb ]
 then
   gdebi --n $deb
 else
-  echo "Did not find .deb for visual studio code: $deb"
+  echo "Did not find .deb for google-chrome: $deb"
 fi
 
+# zoom
+wget --progress=bar --directory-prefix=/tmp https://zoom.us/client/latest/zoom_amd64.deb
+deb=/tmp/zoom_amd64.deb
+if [ -f $deb ]
+then
+  gdebi --n $deb
+else
+  echo "Did not find .deb for zoom: $deb"
+fi
+
+
+
+
+rm -f -v /tmp/*.deb
+echo "[finished installing software]"
