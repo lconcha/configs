b82a07e15d835d2d600e7729a8df2f2bbed4cea4
Luis Concha <lconcha@gmail.com>
2023-05-23 14:09:12 -0600

    deleted mantenimiento folder, moved scripts to admin_tools

--- scripts/mantenimiento/fmrilab_mount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $# -eq 0 ]
-        then
-        echo "Falta punto de montaje"
-        exit 0
-fi
-sudo mount /datos/$1
--- scripts/mantenimiento/fmrilab_umount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $# -eq 0 ]
-        then
-        echo "Falta carpeta a desmontar"
-        exit 0
-fi
-umount -l /datos/$1
