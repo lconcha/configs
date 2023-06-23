4b297ce9f5c70d3a7b658605d9bf402f17578fef
Ricardo Rios <ricardo.rios@cimat.mx>
2023-06-22 18:25:31 -0600

    add sh extension so it can run from profile.d

--- fmrilab_profile.sh
+++ fmrilab_profile.sh
@@ -1,4 +1,4 @@
-
+# This file runs initial configuration for a don_clusterio session
 
 if [ $(id -gn) = "fmriuser" ]; then
 	export FMRILAB_CONFIGFILE=/home/inb/soporte/admin_tools/fmrilab_configfile
