48c6ed959b84eb8e8de3ee733834842e885a36f5
Ricardo Rios <ricardo.rios@cimat.mx>
2023-09-14 14:06:28 -0600

    comment apt install of R and erase rstudio from deb-get software

--- fmrilab_softwareconfig.sh
+++ fmrilab_softwareconfig.sh
@@ -42,19 +42,18 @@ apt -y autoremove
 # PPA SOFTWARE
 
 ## Installing R base
-
 	# update indices
-	apt update -qq
+	#apt update -qq
 	# install two helper packages we need
-	apt -y install --no-install-recommends software-properties-common dirmngr
+	#apt -y install --no-install-recommends software-properties-common dirmngr
 	# add the signing key (by Michael Rutter) for these repos
 	# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 	# Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
-	wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+	#wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 	# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-	add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+	#add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
-	apt -y install --no-install-recommends r-base r-base-dev
+	#apt -y install --no-install-recommends r-base r-base-dev
 
 
 
@@ -70,7 +69,7 @@ curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | su
 apt install ttf-mscorefonts-installer -y
 
 # install deb-get software
-deb-get install rstudio code \
+deb-get install code \
 	rclone \
 	github-desktop \
 	zotero \
